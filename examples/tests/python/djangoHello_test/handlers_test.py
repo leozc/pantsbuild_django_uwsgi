bc1de0f9b660cb55440aef3c3aa9df489b918c88
Leo Liang <leozc@leozc.com>
2016-02-27 21:44:56 -0800

    Touch up.

--- examples/tests/python/djangoHello_test/handlers_test.py
+++ examples/tests/python/djangoHello_test/handlers_test.py
@@ -5,21 +5,34 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import sys
-import os
 import unittest
-from django.test import RequestFactory
 
+import os
+
+from django.test import RequestFactory
 from djangoHello.svc_handlers import hello
 
-# making django happy!
+from greet.greet import greet
+
+# make django happy!
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoHello.settings")
 
 class HandlersTest(unittest.TestCase):
   request_factory = RequestFactory()
-  def test_hello(self):
+  def test_hello_with_param(self):
     request = HandlersTest.request_factory.get('/hello', data={'name': u'foo'})
     resp = hello(request)
     assert('hello' in resp.content)
     assert('foo' in resp.content)
     assert(200 == resp.status_code)
+
+  def test_hello_without_param(self):
+    request = HandlersTest.request_factory.get('/hello', )
+    resp = hello(request)
+    assert('hello' in resp.content)
+    assert('UNKNOWN' in resp.content)
+    assert(200 == resp.status_code)
+
+  def test_greeting_mentions_addressee(self):
+    """Fancy formatting should not omit the person we're greeting"""
+    assert('foo' in greet('foo'))
